54c568ba0e43c781032ae24b41453571cb021419
aRunner <arunner@live.com>
2018-03-29 10:34:12 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/cn/arunner/coolweather/WeatherActivity.java
+++ app/src/main/java/cn/arunner/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package cn.arunner.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -24,6 +25,7 @@ import java.io.IOException;
 
 import cn.arunner.coolweather.gson.Forecast;
 import cn.arunner.coolweather.gson.Weather;
+import cn.arunner.coolweather.service.AutoUpdateService;
 import cn.arunner.coolweather.util.HttpUtil;
 import cn.arunner.coolweather.util.Utility;
 import okhttp3.Call;
@@ -213,5 +215,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
